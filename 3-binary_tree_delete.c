5f8d3c5357cb00a0068863a820d7200298932395
johhnmunene <edwardsjohnny64@gmail.com>
2023-11-02 18:22:00 +0300

    betty and code output

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,14 +7,13 @@
  * Return: nothing
  */
 
-void binary_tree_delete(binary_tree_t *tree) 
+void binary_tree_delete(binary_tree_t *tree)
 {
-	if(tree != NULL) {
+	if (tree != NULL)
+	{
 		return;
-	
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
 	}
-	
-	free(tree);
 }
