ad99f7a655b815c57277ac19f205349f138f5e74
johhnmunene <edwardsjohnny64@gmail.com>
2023-11-02 18:29:47 +0300

    binary_tree_is_leaf code output

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,13 +7,12 @@
  * if node is null, return 0
  */
 
-int binary_tree_is_leaf(const binary_tree_t *node) {
-    if (node == NULL) 
-        return (0); 
-    
-
-    if(node->left == NULL && node->right == NULL)
-     		return(1);
-    return(0);
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	return (node->left == NULL && node->right == NULL);
 
 }
